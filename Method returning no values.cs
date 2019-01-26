46e552479d8cbdcc424a8680ab7a1c2a99e7dbc9
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-26 17:37:48 -0500

    Add files via upload
    I created a method that returns no values.

---
+++ Method returning no values.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Method_returning_a_value
+{
+    class simplemethodreturningvalue
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 10.0, num2 = 20.0, myAnswer;
+            //call the method and assign the return value to varible myAnswer
+            myAnswer = ans(num1, num2);
+            Console.WriteLine("The value returned is " + myAnswer);
+        }
+        public static double ans(double x, double y)
+        {
+            double answer = x + y;
+            return answer;
+        }
+
+    }
+}
